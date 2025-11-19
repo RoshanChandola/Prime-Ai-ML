3124e644ab83c7b4d63f1e7b05ac622c081f670c
Roshan Chandola <roshanchandola2006@gmail.com>
2025-11-19 20:44:52 +0530

    revising the basic concepts of python and getting ready to start ai and ml

---
+++ Basic_python/Input.py
@@ -0,0 +1,5 @@
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+c=a+b
+print("The sum is:",c)
+# input function is used to take user input from the console.it always returns data in string format. Hence type casting is used to convert the string input to integer before performing addition.int(input("Enter first number: "))
---
+++ Basic_python/type_caste.py
@@ -0,0 +1,10 @@
+a=5.7
+b=int(a)
+print(a)  # Output: 5.7
+print(b)  # Output: 5
+#type casting float to int 
+r="2345"
+s=int(r)
+print(r)  # Output: "2345"
+print(s)  # Output: 2345
+#type casting string to int
