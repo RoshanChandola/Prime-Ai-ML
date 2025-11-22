208df4255328e6590815c7bf8f1b73b0d1f81b5d
Roshan Chandola <roshanchandola2006@gmail.com>
2025-11-22 20:48:01 +0530

    first commit

--- Basic_python/Input.py
+++ Basic_python/Input.py
@@ -3,3 +3,5 @@ b=int(input("Enter second number: "))
 c=a+b
 print("The sum is:",c)
 # input function is used to take user input from the console.it always returns data in string format. Hence type casting is used to convert the string input to integer before performing addition.int(input("Enter first number: "))
+x =10+3*2**2
+print(x)
